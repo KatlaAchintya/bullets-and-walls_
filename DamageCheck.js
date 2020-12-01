745511ebf66d17fb57c314c6e78e5cf7fad39a8c
KatlaAchintya <73953374+KatlaAchintya@users.noreply.github.com>
2020-12-01 13:46:49 +0530

    Add files via upload

---
+++ DamageCheck.js
@@ -0,0 +1,14 @@
+function Damage_Check(){
+
+var damage = (0.5 * weight * speed * speed)/(thickness*thickness*thickness)
+if (damage>10){
+
+
+  wall.shapeColor = color(255,0,0)
+}
+if (damage<10){
+
+
+wall.shapeColor = color(0,255,0)
+}
+}
